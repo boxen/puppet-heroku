16a7ec59d15d951d95091852e7cf46c59d502232
Tim Sharpe <tim@github.com>
2012-10-04 16:14:01 -0700

    tomdoc heroku::plugin type

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,3 +1,13 @@
+# Public: Install a heroku plugin.
+#
+# namevar - The name of the plugin.
+# source  - The location of the git repository containing the plugin.
+#
+# Examples
+#
+#   heroku::plugin { 'foo':
+#     source => 'https://github.com/heroku/foo',
+#   }
 define heroku::plugin($source) {
   require heroku::config
 
