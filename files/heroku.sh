f020b25a7cf3f7d9591b1d7fa1d1d18e7ee8ba45
Will Farrington <wfarr@github.com>
2013-03-29 15:36:32 -0400

    Add Heroku Toolbelt installer

---
+++ files/heroku.sh
@@ -0,0 +1,3 @@
+# Add Heroku bins to PATH
+
+export PATH="$BOXEN_HOME/heroku/bin:$PATH"
---
+++ manifests/init.pp
@@ -0,0 +1,27 @@
+# Public: Install Heroku toolbelt.
+#
+# Usage:
+#
+#   include heroku
+
+class heroku {
+  include boxen::config
+
+  $root = "${boxen::config::home}/heroku"
+  
+  $curl = 'curl -s http://assets.heroku.com.s3.amazonaws.com/heroku-client/heroku-client.tgz'
+  $tar  = 'tar zxv - --strip-components 1'
+
+  file {
+    $root:
+      ensure => directory ;
+    "${boxen::config::envdir}/heroku.sh":
+      source => 'puppet:///modules/heroku/heroku.sh' ;
+  }
+  
+  exec { 'install heroku standalone toolbelt':
+    command => "${curl} | ${tar}",
+    cwd     => $root,
+    creates => "${root}/bin/heroku"
+  }  
+}
